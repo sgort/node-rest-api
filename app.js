a2ac82b950f701ea9b0841a72dd10c327a02032b
datafluisteraar <steven.gort@ictu.nl>
2022-02-03 11:34:13 +0100

    parsing the body & handling CORS

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 const res = require('express/lib/response');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Handling CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, Conten-Type, Accept, Authorization'
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Mehods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+})
 
 // Routes that handle requests
 app.use('/products', productRoutes);
